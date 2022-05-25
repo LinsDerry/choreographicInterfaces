4b83003c6ca423326f018c9a42b3d55d7d6b45d0
Jordan Kruguer <Jordan.Kruguer@gmail.com>
2022-05-25 15:10:36 -0400

    Add files via upload

--- version 2.0/mediapipe-holistic-detection-V2.py
+++ version 2.0/mediapipe-holistic-detection-V2.py
@@ -1,15 +1,33 @@
 import cv2
 import mediapipe as mp
+from mediapipe.python.solutions import pose as mp_pose
 import time
 
 import pyautogui, sys
 import numpy as np
 import math
 
+import numpy as np
+import os
+import module
+
 
 mp_drawing = mp.solutions.drawing_utils
 mp_holistic = mp.solutions.holistic
 
+# Folder with pose class CSVs. 
+pose_samples_folder = 'poses_csvs_out'
+
+
+# Initialize embedder.
+pose_embedder = module.FullBodyPoseEmbedder()
+
+# Initialize classifier.
+pose_classifier = module.PoseClassifier(
+    pose_samples_folder=pose_samples_folder,
+    pose_embedder=pose_embedder,
+    top_n_by_max_distance=30,
+    top_n_by_mean_distance=10)
 
 cap = cv2.VideoCapture(0)
 
@@ -51,13 +69,29 @@ with mp_holistic.Holistic(
         #    image, results.face_landmarks, mp_holistic.FACE_CONNECTIONS)
         mp_drawing.draw_landmarks(
             image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
-        #mp_drawing.draw_landmarks(
-        #    image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
-        #mp_drawing.draw_landmarks(
-        #    image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS)
+        mp_drawing.draw_landmarks(
+            image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+        mp_drawing.draw_landmarks(
+            image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS)
 
+        ### POSE ### 
 
+        # Classify the pose on the current frame.
+        output_frame = image.copy()
+        pose_landmarks = results.pose_landmarks
+        if pose_landmarks is not None:
+            # Get landmarks.
+            frame_height, frame_width = output_frame.shape[0], output_frame.shape[1]
+            pose_landmarks = np.array([[lmk.x * frame_width, lmk.y * frame_height, lmk.z * frame_width]
+                                         for lmk in pose_landmarks.landmark], dtype=np.float32)
+            assert pose_landmarks.shape == (33, 3), 'Unexpected landmarks shape: {}'.format(pose_landmarks.shape)
 
+            # Classify the pose on the current frame.
+            pose_classification = pose_classifier(pose_landmarks)
+            print(pose_classification)
+            cv2.putText(image, f'Pose: {pose_classification}', (20,70), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,255,0), 2)
+            action = max(pose_classification, key=pose_classification.get)
+            print(action)
 
         end = time.time()
         totalTime = end - start
@@ -65,7 +99,7 @@ with mp_holistic.Holistic(
         fps = 1 / totalTime
         print("FPS: ", fps)
 
-        cv2.putText(image, f'FPS: {int(fps)}', (20,70), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,255,0), 2)
+        cv2.putText(image, f'FPS: {int(fps)}', (20,120), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,255,0), 2)
 
         cv2.imshow('MediaPipe Holistic', image)
 
@@ -89,31 +123,51 @@ with mp_holistic.Holistic(
         except Exception as e:
             print(e)
 
-        # scroll 
+
         try:
-            pyautogui.scroll(z)
+            if pose_classification[action] == 10: #  Strong Confidence:
+                #pyautogui.PAUSE = 2
+                pyautogui.hotkey('command', 'r') # refresh
+                cv2.putText(image, f'Command: {action}', (20,150), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,255,0), 2)
+                #pyautogui.PAUSE = 0
         except Exception as e:
             print(e)
 
-
         try:
             # Use pinky and thumb as select --> landmark 20, landmark 4
-            x_thumb = results.left_hand_landmarks.landmark[4].x * screenWidth # scaled
-            y_thumb = results.left_hand_landmarks.landmark[4].y * screenHeight # scaled
+            x_thumb = results.right_hand_landmarks.landmark[4].x * screenWidth # scaled
+            y_thumb = results.right_hand_landmarks.landmark[4].y * screenHeight # scaled
 
-            x_pinky = results.left_hand_landmarks.landmark[20].x * screenWidth # scaled
-            y_pinky = results.left_hand_landmarks.landmark[20].y * screenHeight # scaled
+            x_pinky = results.right_hand_landmarks.landmark[20].x * screenWidth # scaled
+            y_pinky = results.right_hand_landmarks.landmark[20].y * screenHeight # scaled
 
             distance = ((((x_thumb - x_pinky)**2)+((y_thumb-y_pinky)**2))**0.5)
 
             #print(f"Distance between pinky and thumb: {distance}")
             if distance<25: # adjust
                 print('Click!')
-                pyautogui.click(clicks=2)
+                pyautogui.click(button='left')
+                cv2.putText(image, f'Command: click', (20,150), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,255,0), 2)
+
 
             distance_old = distance
-                
 
+
+        except Exception as e:
+            print(e)
+
+
+        try:
+            # Use pinky and thumb as select --> landmark 20, landmark 4
+            wrist = results.right_hand_landmarks.landmark[0].z * screenWidth # scaled
+    
+            if wrist < 0:
+                pyautogui.hotkey('command', '-')
+                cv2.putText(image, f'Command: Zoom In', (20,150), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,255,0), 2)
+
+            elif wrist > 0:
+                pyautogui.hotkey('command', '+')
+                cv2.putText(image, f'Command: Zoom Out', (20,150), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,255,0), 2)
 
         except Exception as e:
             print(e)
