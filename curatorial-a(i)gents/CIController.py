806f85f73926a91f8729b1006e1c4762a668b169
Jordan Kruguer <Jordan.Kruguer@gmail.com>
2022-05-25 16:01:18 -0400

    Controller

--- curatorial-a(i)gents/CIController.py
+++ curatorial-a(i)gents/CIController.py
@@ -1,29 +1,28 @@
 #!/usr/bin/python
-import tkinter, time
+import Tkinter, time
 from subprocess import Popen
 
 Freq = 2500
 Dur = 150
 
-top = tkinter.Tk()
+top = Tkinter.Tk()
 top.title('MapAwareness')
 top.geometry('200x100') # Size 200, 200
 
 def start():
     import os
 #   os.system("python test.py")
-    process = Popen(["python", "DataSensoriumTest.py"])
-  
+    Popen(["python", "DataSensoriumTest.py"])
 
 def stop():
     print ("Stop")
-    Popen.kill(["python", "DataSensoriumTest.py"])
-    #top.destroy() # destroy window
+    top.destroy()
 
-startButton = tkinter.Button(top, fg='black',bg='black', height=2, width=20, text ="Start", command = start)
-stopButton = tkinter.Button(top, fg='black',bg='black', height=2, width=20, text ="Stop", command = stop)
+startButton = Tkinter.Button(top, height=2, width=20, text ="Start", 
+command = start)
+stopButton = Tkinter.Button(top, height=2, width=20, text ="Stop", 
+command = stop)
 
 startButton.pack()
 stopButton.pack()
 top.mainloop()
-
