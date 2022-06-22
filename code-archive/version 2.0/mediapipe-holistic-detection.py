9674eab9ece222a50f2a3bb72a458621468225db
Jordan Kruguer <jordan@metalmark.xyz)>
2022-06-21 20:51:41 -0400

    updates

--- code-archive/version 2.0/mediapipe-holistic-detection.py
+++ code-archive/version 2.0/mediapipe-holistic-detection.py
@@ -1,33 +1,17 @@
 import cv2
 import mediapipe as mp
-from mediapipe.python.solutions import pose as mp_pose
 import time
 
 import pyautogui, sys
 import numpy as np
 import math
 
-import numpy as np
-import os
-import module
+import tensorflow.keras as tf
 
 
 mp_drawing = mp.solutions.drawing_utils
 mp_holistic = mp.solutions.holistic
 
-# Folder with pose class CSVs. 
-pose_samples_folder = 'poses_csvs_out'
-
-
-# Initialize embedder.
-pose_embedder = module.FullBodyPoseEmbedder()
-
-# Initialize classifier.
-pose_classifier = module.PoseClassifier(
-    pose_samples_folder=pose_samples_folder,
-    pose_embedder=pose_embedder,
-    top_n_by_max_distance=30,
-    top_n_by_mean_distance=10)
 
 cap = cv2.VideoCapture(0)
 
@@ -69,29 +53,13 @@ with mp_holistic.Holistic(
         #    image, results.face_landmarks, mp_holistic.FACE_CONNECTIONS)
         mp_drawing.draw_landmarks(
             image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
-        mp_drawing.draw_landmarks(
-            image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+        #mp_drawing.draw_landmarks(
+        #    image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
         mp_drawing.draw_landmarks(
             image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS)
 
-        ### POSE ### 
 
-        # Classify the pose on the current frame.
-        output_frame = image.copy()
-        pose_landmarks = results.pose_landmarks
-        if pose_landmarks is not None:
-            # Get landmarks.
-            frame_height, frame_width = output_frame.shape[0], output_frame.shape[1]
-            pose_landmarks = np.array([[lmk.x * frame_width, lmk.y * frame_height, lmk.z * frame_width]
-                                         for lmk in pose_landmarks.landmark], dtype=np.float32)
-            assert pose_landmarks.shape == (33, 3), 'Unexpected landmarks shape: {}'.format(pose_landmarks.shape)
 
-            # Classify the pose on the current frame.
-            pose_classification = pose_classifier(pose_landmarks)
-            print(pose_classification)
-            cv2.putText(image, f'Pose: {pose_classification}', (20,70), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,255,0), 2)
-            action = max(pose_classification, key=pose_classification.get)
-            print(action)
 
         end = time.time()
         totalTime = end - start
@@ -99,7 +67,7 @@ with mp_holistic.Holistic(
         fps = 1 / totalTime
         print("FPS: ", fps)
 
-        cv2.putText(image, f'FPS: {int(fps)}', (20,120), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,255,0), 2)
+        cv2.putText(image, f'FPS: {int(fps)}', (20,70), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,255,0), 2)
 
         cv2.imshow('MediaPipe Holistic', image)
 
@@ -115,7 +83,7 @@ with mp_holistic.Holistic(
             # Anchor on right index finger --> landmark 8
             x = results.left_hand_landmarks.landmark[8].x * screenWidth # scaled
             y = results.left_hand_landmarks.landmark[8].y * screenHeight # scaled
-            z = results.left_hand_landmarks.landmark[8].z
+           #z = results.left_hand_landmarks.landmark[8].z
 
             # Move cursor
             pyautogui.moveTo(x, y)
@@ -123,55 +91,102 @@ with mp_holistic.Holistic(
         except Exception as e:
             print(e)
 
+        # scroll 
+        #try:
+        #    pyautogui.scroll(z)
+        #except Exception as e:
+        #    print(e)
 
-        try:
-            if pose_classification[action] == 10: #  Strong Confidence:
-                #pyautogui.PAUSE = 2
-                pyautogui.hotkey('command', 'r') # refresh
-                cv2.putText(image, f'Command: {action}', (20,150), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,255,0), 2)
-                #pyautogui.PAUSE = 0
-        except Exception as e:
-            print(e)
 
+        '''
         try:
             # Use pinky and thumb as select --> landmark 20, landmark 4
-            x_thumb = results.right_hand_landmarks.landmark[4].x * screenWidth # scaled
-            y_thumb = results.right_hand_landmarks.landmark[4].y * screenHeight # scaled
+            x_thumb = results.left_hand_landmarks.landmark[4].x * screenWidth # scaled
+            y_thumb = results.left_hand_landmarks.landmark[4].y * screenHeight # scaled
 
-            x_pinky = results.right_hand_landmarks.landmark[20].x * screenWidth # scaled
-            y_pinky = results.right_hand_landmarks.landmark[20].y * screenHeight # scaled
+            x_pinky = results.left_hand_landmarks.landmark[20].x * screenWidth # scaled
+            y_pinky = results.left_hand_landmarks.landmark[20].y * screenHeight # scaled
 
             distance = ((((x_thumb - x_pinky)**2)+((y_thumb-y_pinky)**2))**0.5)
 
             #print(f"Distance between pinky and thumb: {distance}")
             if distance<25: # adjust
                 print('Click!')
-                pyautogui.click(button='left')
-                cv2.putText(image, f'Command: click', (20,150), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,255,0), 2)
-
+                pyautogui.click(clicks=2)
 
             distance_old = distance
+                
 
 
         except Exception as e:
             print(e)
 
+        '''
 
-        try:
-            # Use pinky and thumb as select --> landmark 20, landmark 4
-            wrist = results.right_hand_landmarks.landmark[0].z * screenWidth # scaled
-    
-            if wrist < 0:
-                pyautogui.hotkey('command', '-')
-                cv2.putText(image, f'Command: Zoom In', (20,150), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,255,0), 2)
+        ########################
+        # Pose detection (Old) #
+        ########################
 
-            elif wrist > 0:
-                pyautogui.hotkey('command', '+')
-                cv2.putText(image, f'Command: Zoom Out', (20,150), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,255,0), 2)
+        # load the teachable machine model
 
-        except Exception as e:
-            print(e)
+        # read .txt file to get labels
+        labels_path = './labels.txt' 
+        # open input file label.txt
+        labelsfile = open(labels_path, 'r')
 
+        # initialize classes and read in lines until there are no more
+        classes = []
+        line = labelsfile.readline()
+        while line:
+            # retrieve just class name and append to classes
+            classes.append(line.split(' ', 1)[1].rstrip())
+            line = labelsfile.readline()
+        # close label file
+        labelsfile.close()
+
+        model_path = './keras_model.h5' 
+        model = tf.models.load_model(model_path, compile=False)
+
+
+        data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+        frame = cv2.flip(image, 1)
+
+        frameWidth = screenWidth
+        frameHeight = screenHeight
+
+        # crop to square for use with TM model
+        margin = int(((frameWidth-frameHeight)/2))
+        square_frame = frame[0:frameHeight, margin:margin + frameHeight]
+        # resize to 224x224 for use with TM model
+        resized_img = cv2.resize(square_frame, (224, 224))
+        # convert image color to go to model
+        model_img = cv2.cvtColor(resized_img, cv2.COLOR_BGR2RGB)
+
+        # turn the image into a numpy array
+        image_array = np.asarray(model_img)
+        # normalize the image
+        normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+        # load the image into the array
+        data[0] = normalized_image_array
+
+        # run the prediction
+        predictions = model.predict(data)
+
+
+
+        # confidence threshold is 90%.
+        conf_threshold = 95
+        confidence = []
+
+        for i in range(0, len(classes)):
+            
+            # scale prediction confidence to % and apppend to 1-D list
+            confidence.append(int(predictions[0][i]*100))
+
+            if confidence[i] > conf_threshold:
+                threshold_class = classes[i]
+                print(f'Class: {threshold_class}\n Confidence {confidence[i]}\n')
+                #cv2.putText(image, f'Keras Class: {threshold_class}', (20,70), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,255,0), 2)
 
 
 
