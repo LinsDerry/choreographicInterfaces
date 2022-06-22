8af2eae0f44991d74c56c22effa943d26cafde46
Jordan Kruguer <jordan@metalmark.xyz)>
2022-06-21 21:27:35 -0400

    versioning

--- code-archive/version 2.0/mediapipe-holistic-detection.py
+++ code-archive/version 2.0/mediapipe-holistic-detection.py
@@ -8,30 +8,37 @@ import numpy as np
 import math
 
 import os
-import module # RENAME
+import pose_module # RENAME
 
+t0 = time.time()
 
 mp_drawing = mp.solutions.drawing_utils
 mp_holistic = mp.solutions.holistic
+
+lav = (187, 160, 178) # RGB for mp_drawing
+pink = (172, 18, 143) # RGB for mp_drawing
+red = (58, 45, 240) # BGR for cv2 text
+white = (0, 0, 0)
+black = (0, 0, 0)
 
 # Folder with pose class CSVs. 
 pose_samples_folder = 'poses_csvs_out'
 
 # Initialize embedder.
-pose_embedder = module.FullBodyPoseEmbedder()
+pose_embedder = pose_module.FullBodyPoseEmbedder()
 
 # Initialize classifier.
-pose_classifier = module.PoseClassifier(
+pose_classifier = pose_module.PoseClassifier(
     pose_samples_folder=pose_samples_folder,
     pose_embedder=pose_embedder,
     top_n_by_max_distance=30,
     top_n_by_mean_distance=10)
 
 cap = cv2.VideoCapture(0)
-lav = (187, 160, 178) # RGB for mp_drawing
-pink = (172, 18, 143) # RGB for mp_drawing
-red = (58, 45, 240) # BGR for cv2 text
-white = (0, 0, 0)
+
+t1 = time.time()
+print(f"Took {t1-t0} seconds for setup")
+
 
 with mp_holistic.Holistic(
     min_detection_confidence=0.5,
@@ -40,6 +47,7 @@ with mp_holistic.Holistic(
     while cap.isOpened():
         
 
+        t0 = time.time()
         success, image = cap.read()
        
         start = time.time()
@@ -61,6 +69,8 @@ with mp_holistic.Holistic(
         # Convert the image color back so it can be displayed
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
+        t1 = time.time()
+        print(f"Took {t1-t0} seconds to get frame and classifiy using holistic model")
 
         #----------------#
         # DRAW LANDMARKS #
@@ -70,6 +80,7 @@ with mp_holistic.Holistic(
         rad = 9
         thick = 4
 
+        # t0 = time.time()
         # mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACE_CONNECTIONS)
         mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS, mp_drawing.DrawingSpec(color=lmColor, thickness=thick, circle_radius=(rad // 2)),
                                  mp_drawing.DrawingSpec(color=connColor, thickness=thick, circle_radius=rad))
@@ -77,21 +88,29 @@ with mp_holistic.Holistic(
                                  mp_drawing.DrawingSpec(color=connColor, thickness=thick, circle_radius=rad))
         mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS, mp_drawing.DrawingSpec(color=lmColor, thickness=thick, circle_radius=rad),
                                  mp_drawing.DrawingSpec(color=connColor, thickness=thick, circle_radius=rad))
+        # t1 = time.time()
+        # print(f"Took {t1-t0} seconds to draw landmarks")
         
 
         #---------------#
         # CLASSIFY POSE #
         #---------------#
+        t0 = time.time()
         output_frame = image.copy()
         pose_landmarks = results.pose_landmarks
         if pose_landmarks is not None:
             frame_height, frame_width = output_frame.shape[0], output_frame.shape[1]
             pose_landmarks = np.array([[lmk.x * frame_width, lmk.y * frame_height, lmk.z * frame_width] for lmk in pose_landmarks.landmark], dtype=np.float32)
             assert pose_landmarks.shape == (33, 3), 'Unexpected landmarks shape: {}'.format(pose_landmarks.shape)
+            t1 = time.time()
+            print(f"Took {t1-t0} seconds to format the pose landmarks")
+
             # Classify the pose on the current frame.
             pose_classification = pose_classifier(pose_landmarks)
             action = max(pose_classification, key=pose_classification.get)
             # cv2.putText(image, f'Pose: {action}', (20,70), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,255,0), 2) # provide feedback on current pose
+            t1 = time.time()
+            print(f"Took {t1-t0} seconds to classify the pose")
 
 
         #-----------------#
@@ -133,7 +152,6 @@ with mp_holistic.Holistic(
                 #pyautogui.PAUSE = 0
         except Exception as e:
             print(e)
-         #TEMPLATE END
 
 #         try:
 #             if (action=='select') and (pose_classification[action] == 10): #  Stron Confidence:
@@ -144,21 +162,21 @@ with mp_holistic.Holistic(
 #         except Exception as e:
 #             print(e)
 
-        try:
-            if (action=='zoomIn') and (pose_classification[action] == 10): #& pose_classification[action] == 10: #  Strong Confidence:
-                #pyautogui.PAUSE = 2
-                pyautogui.hotkey('command', '+') #zoom in
-                #pyautogui.PAUSE = 0
-        except Exception as e:
-            print(e)
+        # try:
+        #     if (action=='zoomIn') and (pose_classification[action] == 10): #& pose_classification[action] == 10: #  Strong Confidence:
+        #         #pyautogui.PAUSE = 2
+        #         pyautogui.hotkey('command', '+') #zoom in
+        #         #pyautogui.PAUSE = 0
+        # except Exception as e:
+        #     print(e)
 
-        try:
-            if (action=='zoomOut') and (pose_classification[action] == 10): #& pose_classification[action] == 10: #  Strong Confidence:
-                #pyautogui.PAUSE = 2
-                pyautogui.hotkey('command', '-') #zoom out
-                #pyautogui.PAUSE = 0
-        except Exception as e:
-            print(e)
+        # try:
+        #     if (action=='zoomOut') and (pose_classification[action] == 10): #& pose_classification[action] == 10: #  Strong Confidence:
+        #         #pyautogui.PAUSE = 2
+        #         pyautogui.hotkey('command', '-') #zoom out
+        #         #pyautogui.PAUSE = 0
+        # except Exception as e:
+        #     print(e)
              
         try:
             if (action=='scrollUp') and (pose_classification[action] == 10): #  Stron Confidence:
@@ -191,10 +209,11 @@ with mp_holistic.Holistic(
             if distance<20: # adjust
                 #pyautogui.click(button='left')
                 pyautogui.mouseDown()
-                cv2.putText(image, 'Select', (25,140), cv2.FONT_HERSHEY_TRIPLEX, 1.75, red, 2)
+                cv2.putText(image, 'select', (25,140), cv2.FONT_HERSHEY_TRIPLEX, 1.5, black, 2)
 
-            elif distance<20:
+            elif distance>20:
                 pyautogui.mouseUp()
+                cv2.putText(image, 'unSelect', (25,140), cv2.FONT_HERSHEY_TRIPLEX, 1.5, black, 2)
                 
 
         except Exception as e:
@@ -238,7 +257,7 @@ with mp_holistic.Holistic(
         capHeight = cap.get(4)
 
         # Scale feedback
-        scale = 0.25
+        scale = 0.5
         newCapWidth = int(screenWidth * (scale))
         newCapHeight = int(newCapWidth * capHeight / capWidth)
 
@@ -246,8 +265,8 @@ with mp_holistic.Holistic(
         capX = screenWidth - newCapWidth - 5
         capY = screenHeight - newCapHeight - 33
 
-        #cv2.putText(image, f'FPS: {int(fps)}', (20,120), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,255,0), 2) # provide feedback on current FPS
-        cv2.putText(image, f'Interactive gesture: {action}', (25,70), cv2.FONT_HERSHEY_TRIPLEX, 1.75, red, 3) # provide feedback on current pose
+        cv2.putText(image, f'FPS: {int(fps)}', (20,120), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,255,0), 2) # provide feedback on current FPS
+        cv2.putText(image, f'{action}', (25,65), cv2.FONT_HERSHEY_TRIPLEX, 1.5, black, 2) # provide feedback on current pose
         cv2.imshow('Choreographic Interface', cv2.resize(image, (newCapWidth, newCapHeight)))
         cv2.setWindowProperty('Choreographic Interface', cv2.WND_PROP_TOPMOST, 1) # keeps feedback window most front
         cv2.moveWindow('Choreographic Interface', capX,capY) # relocate feedback
